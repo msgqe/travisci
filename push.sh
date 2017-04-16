e559035bfcf0cf08a321cabdf3160ccb2c5f5feb
Jiri Danek <jdanek@redhat.com>
2017-04-16 15:30:19 +0200

    stop leaking my docker password in build log
    the problem was that set -x will print executed commands; when these
    include password on command line, it will be printed.

--- push.sh
+++ push.sh
@@ -3,8 +3,6 @@ set -ex
 
 # push built images into docker hub
 
-docker login -u jdanekrh -p ${DOCKER_PASSWORD}
-
 docker tag  jdanekrh/dispatch-console:latest jdanekrh/dispatch-console:nightly-build-$TRAVIS_BUILD_NUMBER
 docker tag  jdanekrh/dispatch-router:latest jdanekrh/dispatch-router:nightly-build-$TRAVIS_BUILD_NUMBER
 
