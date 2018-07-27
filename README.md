d48817f430e6f12a025369a3489c72b641b4ef57
Jiri Danek <jdanek@redhat.com>
2018-07-27 17:27:48 +0200

    Initial commit of Apache Qpid Proton Coverity build config

--- README.md
+++ README.md
@@ -1,9 +1,9 @@
 # activemq-artemis-coverity
 
-[![Travis CI Build Status](https://travis-ci.org/msgqe/travisci.svg?branch=activemq-artemis-coverity)](https://travis-ci.org/msgqe/travisci/branches)
-[![Coverity Scan Status](https://scan.coverity.com/projects/11809/badge.svg)](https://scan.coverity.com/projects/apacheactivemqartemis)
+[![Travis CI Build Status](https://travis-ci.org/msgqe/travisci.svg?branch=qpid-proton-coverity)](https://travis-ci.org/msgqe/travisci/branches)
+[![Coverity Scan Status](https://scan.coverity.com/projects/4486/badge.svg)](https://scan.coverity.com/projects/apache-qpid-proton)
 
-Builds activemq-artemis and submits the build to Coverity Scan
+Builds qpid-proton and submits the build to Coverity Scan
 
 Click the badges above to see the latest results.
 
--- build_for_analysis.sh
+++ build_for_analysis.sh
@@ -1,13 +1,27 @@
 #!/usr/bin/env bash
+#
+# Licensed to the Apache Software Foundation (ASF) under one
+# or more contributor license agreements.  See the NOTICE file
+# distributed with this work for additional information
+# regarding copyright ownership.  The ASF licenses this file
+# to you under the Apache License, Version 2.0 (the
+# "License"); you may not use this file except in compliance
+# with the License.  You may obtain a copy of the License at
+#
+#   http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing,
+# software distributed under the License is distributed on an
+# "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+# KIND, either express or implied.  See the License for the
+# specific language governing permissions and limitations
+# under the License.
+#
 
-# build everything that should be analyzed
+# build_for_analysis.sh builds everything that should be analyzed
 
-cd activemq-artemis
-
-pushd artemis-native/
-./compile-native.sh
-popd
-
-# https://scan.coverity.com/download?tab=java
-# see ARTEMIS-861, Coverity Scan has similar limitations as IBM JDK, by coincidence
-mvn -Pibm-jdk8 -DskipTests=true -Dmaven.javadoc.skip=true -pl \!:joram-tests,\!:soak-tests,\!:stress-tests,\!:timing-tests,\!:performance-tests,\!:integration-tests,\!:jms-tests,\!:smoke-tests,\!:compatibility-tests compile
+cd qpid-proton/build
+# https://scan.coverity.com/download?tab=cxx
+# https://scan.coverity.com/download?tab=other
+cmake .. -DCMAKE_INSTALL_PREFIX=$PWD/install
+cmake --build . --target install
