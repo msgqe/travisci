42ecdd5c682250004aaf2a3f92a7af4a4316b9e5
Jiri Danek <jdanek@redhat.com>
2019-06-24 13:20:56 +0200

    update for activemq-artemis-native being in repo of its own

--- build_for_analysis.sh
+++ build_for_analysis.sh
@@ -3,12 +3,11 @@ set -Eeuxo pipefail
 
 # build everything that should be analyzed
 
-cd activemq-artemis
-
-pushd artemis-native/
-./compile-native.sh
+pushd activemq-artemis-native
+./scripts/compile-native.sh
 popd
 
+cd activemq-artemis
 # https://scan.coverity.com/download?tab=java
 # see ARTEMIS-861, Coverity Scan has similar limitations as IBM JDK, by coincidence
 mvn -Pibm-jdk8 -DskipTests=true -Dmaven.javadoc.skip=true -pl \!:joram-tests,\!:soak-tests,\!:stress-tests,\!:timing-tests,\!:performance-tests,\!:integration-tests,\!:jms-tests,\!:smoke-tests,\!:compatibility-tests compile
--- prepare_for_build.sh
+++ prepare_for_build.sh
@@ -4,10 +4,4 @@ set -Eeuxo pipefail
 # clone the repository and clean up things
 
 git clone https://github.com/apache/activemq-artemis.git
-
-cd activemq-artemis
-
-pushd artemis-native
-cmake .
-make clean
-popd
+git clone https://github.com/apache/activemq-artemis-native.git
